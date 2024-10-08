c195e5b9d96def426fa781db8709acf8700d678e
“naandalist” <listiananda.apriliawan@gmail.com>
2024-10-09 00:34:35 +0700

    feat: code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import { cors } from "hono/cors";
-import mushroomsData from "./data/mushrooms.json";
+import { logger } from "hono/logger";
+import dataMushrooms from "./data/mushrooms.json";
 
 const app = new Hono();
 
 app.use("/*", cors());
+app.use(logger());
 
 // Root route
 app.get("/", (c) => {
@@ -14,51 +16,57 @@ app.get("/", (c) => {
     endpoints: [
       "/api/mushrooms",
       "/api/mushrooms/:id",
-      "/api/mushrooms/search/:query",
+      "/api/mushrooms/search",
     ],
   });
 });
 
 // Get all mushrooms
 app.get("/api/mushrooms", (c) => {
-  const conciseMushrooms = mushroomsData.map((mushroom) => {
-    return {
-      id: mushroom.id,
-      name: mushroom.name,
-      commonName: mushroom.commonName,
-      family: mushroom.family,
-      edibility: mushroom.edibility,
-      shortDescription: mushroom.shortDescription,
-      habitat: mushroom.habitat,
-      season: mushroom.season,
-      funFact: mushroom.funFact,
-    };
-  });
-  return c.json(conciseMushrooms);
+  const mushrooms = dataMushrooms.map(({ longDescription, ...rest }) => rest);
+  return c.json({ status: 200, message: "success", data: mushrooms });
+});
+
+// Search mushrooms by name or common name
+app.get("/api/mushrooms/search", (c) => {
+  const query = c.req.query("q");
+
+  console.log("query: ", query);
+
+  if (!query) {
+    return c.json(
+      { status: 404, message: "Search query is required", data: [] },
+      400
+    );
+  }
+
+  const lowercaseQuery = query.toLowerCase();
+  const mushrooms = dataMushrooms.filter(
+    (item) =>
+      item.name.toLowerCase().includes(lowercaseQuery) ||
+      item.commonName.toLowerCase().includes(lowercaseQuery)
+  );
+
+  if (mushrooms.length === 0) {
+    return c.json(
+      { status: 200, message: "No mushrooms found", data: [] },
+      200
+    );
+  }
+
+  return c.json({ status: 200, message: "success", data: mushrooms });
 });
 
 // Get one mushroom by ID
 app.get("/api/mushrooms/:id", (c) => {
   const id = c.req.param("id");
-  const mushroom = mushroomsData.find((m) => m.id === id);
+  const mushroom = dataMushrooms.find((item) => item.id === id);
 
-  if (mushroom) {
-    return c.json(mushroom);
-  } else {
+  if (!mushroom) {
     return c.json({ error: "Mushroom not found" }, 404);
   }
-});
 
-// Search mushrooms by name or common name
-app.get("/api/mushrooms/search/:query", (c) => {
-  const query = c.req.param("query").toLowerCase();
-  const results = mushroomsData.filter(
-    (m) =>
-      m.name.toLowerCase().includes(query) ||
-      m.commonName.toLowerCase().includes(query)
-  );
-
-  return c.json(results);
+  return c.json({ status: 200, message: "success", mushroom });
 });
 
 export default app;
